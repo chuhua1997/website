977a0fe87c5dbaeeae3bf698edff7813e6c00966
chuhua1997 <chuhua1005@gmail.com>
2024-04-23 21:06:14 +0800

    商品個別頁面對齊

--- js/script.js
+++ js/script.js
@@ -5,10 +5,7 @@ var swiper = new Swiper(".mySwiper", {
     keyboard: {
         enabled: true,
     },
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
