af9d5f5deb54268303fa2919e03d510e8ff032e7
chuhua1997 <chuhua1005@gmail.com>
2024-05-04 20:21:37 +0800

    成功叫出下拉式選單

--- js/nav-set.js
+++ js/nav-set.js
@@ -1,11 +1,8 @@
 // 點按漢堡會出現選單
 $(document).ready(function () {
     $('#menu-switch').click(function () {
-        $('.pc-nav').addClass('show-menu');
-        alert($('.show-menu').hasClass('show-menu'))
-
-        // $('.pc-nav').show();
-        // $('.pc-nav').text(123);
+        $('.nav-selector').addClass('show-menu');
+        // alert($('.nav-selector').hasClass('show-menu'))
         $('.show-menu').toggle()
     });
 });
