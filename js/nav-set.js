740124dc88cad7d91a5e9c0dd2b896dae49c388b
chuhua1997 <chuhua1005@gmail.com>
2024-05-04 19:44:13 +0800

    可出現漢堡

--- js/nav-set.js
+++ js/nav-set.js
@@ -1,11 +1,11 @@
 // 點按漢堡會出現選單
 $(document).ready(function () {
     $('#menu-switch').click(function () {
-        // $('.pc-nav').addClass('show-menu');
-        alert($('.pc-nav').hasClass('show-menu'))
+        $('.pc-nav').addClass('show-menu');
+        alert($('.show-menu').hasClass('show-menu'))
 
         // $('.pc-nav').show();
         // $('.pc-nav').text(123);
-        // $('.show-menu').toggle()
+        $('.show-menu').toggle()
     });
 });
