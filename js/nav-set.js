3c991801bffd765f295b79a6044666da1fa63418
chuhua1997 <chuhua1005@gmail.com>
2024-05-07 21:46:52 +0800

    商品頁RWD完成

--- js/nav-set.js
+++ js/nav-set.js
@@ -1,8 +1,9 @@
 // 點按漢堡會出現選單
 $(document).ready(function () {
     $('#menu-switch').click(function () {
-        $('.nav-selector').toggleClass('show-menu');
         // alert($('.nav-selector').hasClass('show-menu'))
+        $('.nav-selector').toggleClass('show-menu');
+
         // $('.show-menu').toggle()
     });
 });
